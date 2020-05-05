da8e423f8d150b2ab49e14eb7c181a4b5de75e5c
kehochman <58151660+kehochman@users.noreply.github.com>
2020-05-05 16:51:40 -0400

    First push

---
+++ static/js/logic.js
@@ -0,0 +1,14 @@
+var myMap = L.map("map", {
+    center: [0, -40],
+    zoom: 3
+  });
+  
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
